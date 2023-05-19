57173602b0368df4c4d72e4057703ffc48e48697
IsratMoon <mnisrat.j@gmail.com>
2023-05-19 06:17:19 +0600

    .env File Added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,13 +2,15 @@
 import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyBoveN7Kk_hEOy_a6o7SnQ79YrmZ0pi58I",
-	authDomain: "toy-quest.firebaseapp.com",
-	projectId: "toy-quest",
-	storageBucket: "toy-quest.appspot.com",
-	messagingSenderId: "1003918577469",
-	appId: "1:1003918577469:web:e883d01800625f826cff54",
+	apiKey: import.meta.env.VITE_APIKEY,
+	authDomain: import.meta.env.VITE_AUTHDOMAIN,
+	projectId: import.meta.env.VITE_PROJECTID,
+	storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+	messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+	appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+
